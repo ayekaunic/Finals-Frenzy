29ed2a4bf3ba95511c8b9274916dcb9a94fad7f9
Muhammad Aun <81079827+ayekaunic@users.noreply.github.com>
2023-04-04 11:03:07 +0500

    Add files via upload

---
+++ finals_frenzy.py
@@ -0,0 +1,27 @@
+from datetime import date
+from email.message import EmailMessage
+import smtplib
+
+finals = date(2022, 12, 11)
+today = date.today()
+days_left = (((finals - today).days) - 1)
+
+sender = 'insert bot account email here'
+receiver = open('mail list.txt').readlines()
+password = 'insert bot account email here'
+
+reminder = EmailMessage()
+if days_left > 1:
+    reminder['Subject'] = f'{days_left} days until Finals!'
+else:
+    reminder['Subject'] = f'{days_left} day until Finals!'
+reminder['From'] = sender
+reminder['Bcc'] = receiver
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+    server.login(sender, password)
+    server.send_message(reminder)
+    server.quit()
+
+# C:\Program Files\Python311\python.exe
+# C:\Users\CC\PycharmProjects\finals reminder\main.py
